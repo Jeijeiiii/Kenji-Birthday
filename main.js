8c857513e0b60b6793e3e05ca1a3fbad8a443384
jeijei <jessicaciu2008@gmail.com>
2025-10-23 23:24:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -271,7 +271,7 @@ const songs = [
     title: "bad - wave to earth",
     cover: "Songs/bad.png",
     audio: "Songs/bad.mp3",
-    lyrics: "How could my day be bad when I'm with you? You're the only one who makes me laugh. So how can my day be bad?"
+    lyrics: "How could my day be bad when I'm with you? You're the only one who makes me laugh."
   },
 ];
 
@@ -552,3 +552,4 @@ polaroidLeftArrow.addEventListener('click', () => slidePolaroid('left'));
 
 
 loadPolaroid(currentPolaroid);
+
